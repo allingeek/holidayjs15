c9e13ac2865a7e954f76aa7ef2b9d5c60ff6b1a8
Jeff Nickoloff <jeff@allingeek.com>
2015-12-12 14:34:42 -0700

    Added Backand config
    Signed-off-by: Jeff Nickoloff <jeff@allingeek.com>

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,8 +6,10 @@
     .config(config);
 
   /** @ngInject */
-  function config() {
-
+  function config(BackandProvider) {
+      BackandProvider.setAppName('holidayjs');
+      BackandProvider.setSignUpToken('beca32ba-ec4f-4658-a6db-25e4ed677d96');
+      BackandProvider.setAnonymousToken('d93fcfeb-4fec-4aad-8e02-64f92e40884a');
   }
 
 })();
