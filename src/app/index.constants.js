71a34c38fc981900dc0d07494fde00abdaff2f47
Kyle Schaffer <kdschaffe@gmail.com>
2015-12-12 11:55:53 -0700

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('holidayjs15')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('holidayjs15', ['ui.router', 'toastr']);
+
+})();
